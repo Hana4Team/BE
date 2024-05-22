4f6741f89420be74d2b7378157b925a42ba9a980
khw18 <gmsb14@naver.com>
2024-05-22 13:35:54 +0900

    Config: init set

---
+++ src/test/java/com/hana/ddok/DdokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hana.ddok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdokApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
