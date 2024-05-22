b3c85ef12015dd7dfd83aa0b08317c06fd9e3f15
khw18 <gmsb14@naver.com>
2024-05-22 13:35:54 +0900

    Config: init set #1

---
+++ src/main/java/com/hana/ddok/DdokApplication.java
@@ -0,0 +1,13 @@
+package com.hana.ddok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdokApplication.class, args);
+    }
+
+}
