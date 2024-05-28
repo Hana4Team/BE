25c98b6632522ba8d5583b3286b5404743bd6f9e
khw18 <gmsb14@naver.com>
2024-05-28 13:39:08 +0900

    Config: sec config  #10

---
+++ src/main/java/com/hana/ddok/users/repository/UsersRepository.java
@@ -0,0 +1,8 @@
+package com.hana.ddok.users.repository;
+
+import com.hana.ddok.users.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+
+}
