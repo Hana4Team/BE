db82456b307e221dbdcd0fdd2db26e337de0a571
eunjin917 <cgoya01@likelion.org>
2024-06-01 22:15:42 +0900

    Feat: Moneybox Entity #38

---
+++ src/main/java/com/hana/ddok/moneybox/domain/Moneybox.java
@@ -0,0 +1,46 @@
+package com.hana.ddok.account.domain;
+
+import com.hana.ddok.common.domain.BaseEntity;
+import com.hana.ddok.products.domain.Products;
+import com.hana.ddok.users.domain.Users;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "moneybox")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Moneybox extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "moneybox_id")
+    private Long moneyboxId;
+
+    @Column(name = "parking_balance", nullable = false)
+    private Long parkingBalance;
+
+    @Column(name = "expense_balance", nullable = false)
+    private Long expenseBalance;
+
+    @Column(name = "saving_balance", nullable = false)
+    private Long savingBalance;
+
+    @Column(name = "expense_total", nullable = false)
+    private Long expenseTotal;
+
+    @Column(name = "is_charged", nullable = false)
+    private Boolean isCharged;
+
+    @OneToOne
+    @JoinColumn(name = "account_id")
+    private Account account;
+}
