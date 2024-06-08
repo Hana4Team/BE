c9132a538fdb86b76c3db49c11ceaf8093ae6a5d
eunjin917 <cgoya01@likelion.org>
2024-06-08 17:10:08 +0900

    Feat: 계좌 개설 수 제한 #101

--- src/main/java/com/hana/ddok/account/repository/AccountRepository.java
+++ src/main/java/com/hana/ddok/account/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.hana.ddok.account.repository;
 
 import com.hana.ddok.account.domain.Account;
+import com.hana.ddok.products.domain.Products;
 import com.hana.ddok.products.domain.ProductsType;
 import com.hana.ddok.users.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,6 +12,7 @@ import java.util.Optional;
 public interface AccountRepository extends JpaRepository<Account, Long> {
     List<Account> findAllByUsers(Users users);
     Optional<Account> findByUsersAndProductsType(Users users, ProductsType type);
+    Optional<Account> findByUsersAndProducts(Users users, Products products);
     List<Account> findAllByUsersAndProductsTypeIn(Users users, List<ProductsType> typeList);
     Optional<Account> findByAccountNumber(String accountNumber);
 }
