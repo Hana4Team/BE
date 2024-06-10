3dddd053f80ef468f1473de7633c271e02e7c7c0
khw18 <gmsb14@naver.com>
2024-06-10 13:57:43 +0900

    Fix: cors #148

--- src/main/java/com/hana/ddok/common/config/WebMvcConfig.java
+++ src/main/java/com/hana/ddok/common/config/WebMvcConfig.java
@@ -12,7 +12,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://localhost:80","http://localhost","http://ec2-43-201-217-228.ap-northeast-2.compute.amazonaws.com","http://ec2-43-201-217-228.ap-northeast-2.compute.amazonaws.com:3000","http://ec2-43-201-217-228.ap-northeast-2.compute.amazonaws.com:8080")
+                .allowedOrigins("https://localhost:3000","https://localhost:80","https://localhost","https://ec2-43-201-217-228.ap-northeast-2.compute.amazonaws.com","https://ec2-43-201-217-228.ap-northeast-2.compute.amazonaws.com:3000","https://ec2-43-201-217-228.ap-northeast-2.compute.amazonaws.com:8080")
                 .allowedMethods("OPTIONS", "GET", "POST", "PUT", "DELETE")
                 .allowCredentials(true);
     }
