a59f9500a8cb7fc0f230a8ab91406ec918d65707
eunjin917 <cgoya01@likelion.org>
2024-06-12 10:36:17 +0900

    Feat: swagger #138

--- src/main/java/com/hana/ddok/spend/controller/SpendContoller.java
+++ src/main/java/com/hana/ddok/spend/controller/SpendContoller.java
@@ -5,6 +5,8 @@ import com.hana.ddok.spend.service.SpendService;
 import com.hana.ddok.transaction.dto.*;
 import com.hana.ddok.transaction.service.TransactionService;
 import com.hana.ddok.users.domain.UsersDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,10 +17,12 @@ import java.util.List;
 @RequestMapping("/api/v1")
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "Spend", description = "지출 API")
 public class SpendContoller {
     private final SpendService spendService;
 
     @GetMapping("spend")
+    @Operation(summary = "지출 전체 조회")
     public ResponseEntity<SpendFindAllRes> spendFindAll(@RequestParam Integer year, @RequestParam Integer month, @AuthenticationPrincipal UsersDetails usersDetails) {
         SpendFindAllRes spendFindAllRes = spendService.spendFindAll(year, month, usersDetails.getUsername());
         return ResponseEntity.ok(spendFindAllRes);
